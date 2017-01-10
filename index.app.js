19a28a725e9dcdcbd38c195b7637e05d751f38af
Jon New <jonnew@gmail.com>
2017-01-09 17:36:20 -0800

    Added link to blog.

--- index.app.js
+++ index.app.js
@@ -7,7 +7,7 @@ nconf.env().file({file: ".env"});
 var port = nconf.get("PORT");
 
 app.get('*', function(req, res) {
-  res.send('The impossible is possible at ' + req.get('host') + req.originalUrl);
+  res.send('The impossible is possible at ' + req.get('host') + req.originalUrl  + "<br/><br/>Also, I have a <a href='http://blog.jonnew.com'>blog</a>.");
 });
 
 app.listen(port, function() {
